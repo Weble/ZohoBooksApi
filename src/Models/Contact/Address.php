162baaa6cb2590af968251bbb6cd73fb784013d6
Chris Nolan <chris@leytech.co.uk>
2019-09-17 16:00:44 +0100

    Added support for getting the addresses of a contact

---
+++ src/Models/Contact/Address.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Webleit\ZohoBooksApi\Models\Contact;
+
+use Webleit\ZohoBooksApi\Models\Model;
+
+/**
+ * Class Person
+ * @package Webleit\ZohoBooksApi\Models\Contact
+ *
+ * @method  bool markAsPrimary()
+ */
+class Address extends Model
+{
+    public function getKeyName()
+    {
+        return 'address_id';
+    }
+}
