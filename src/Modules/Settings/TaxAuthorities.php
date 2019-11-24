9eb8039aea794980dce8a7039b0b2f619d521162
Rob Thomas <rthomas@clearlyip.com>
2019-11-25 06:43:50 +1000

    Fix tax_authoritie(s)

--- src/Modules/Settings/TaxAuthorities.php
+++ src/Modules/Settings/TaxAuthorities.php
@@ -26,4 +26,29 @@ class TaxAuthorities extends Module
     {
         return TaxAuthority::class;
     }
+
+    /**
+     * Return Zoho API name
+     *
+     * This overrides getResourceKey in Module, which normally
+     * returns strtolower of name. However, Zoho returns "tax_authorities"
+     * instead of the expected 'taxauthorities' key.
+     */
+    public function getResourceKey()
+    {
+        return "tax_authorities";
+    }
+
+    /**
+     * Return Zoho API Key Name
+     *
+     * This is the unique key used to return data. With the taxauthorities
+     * module, it returns the data as an array in 'tax_authorities' but the
+     * actual key is 'tax_authority_id', without the trailing s.
+     */
+    public function getApiKeyName()
+    {
+        return "tax_authoritiy_id";
+    }
 }
+
