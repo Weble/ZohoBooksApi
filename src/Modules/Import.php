9b435da321602fb9ecb5c224e33df41636305844
Skullbock <daniele@weble.it>
2020-06-22 09:30:24 +0200

    updated code for import from CRM

---
+++ src/Modules/Import.php
@@ -0,0 +1,67 @@
+<?php
+namespace Webleit\ZohoBooksApi\Modules;
+
+
+use Webleit\ZohoBooksApi\Exceptions\ErrorResponseException;
+use Webleit\ZohoBooksApi\Models\Contact;
+
+class Import extends Module
+{
+    /**
+     * @param $crm_contact_id
+     * @return Contact
+     * @throws ErrorResponseException
+     */
+    public function contact($crm_contact_id): Contact
+    {
+        return $this->importFromCRM($crm_contact_id, 'contact');
+    }
+
+
+    /**
+     * @param $crm_account_id
+     * @return Contact
+     * @throws ErrorResponseException
+     */
+    public function account($crm_account_id): Contact
+    {
+        return $this->importFromCRM($crm_account_id, 'account');
+    }
+
+
+    /**
+     * @param $crm_vendor_id
+     * @return Contact
+     * @throws ErrorResponseException
+     */
+    public function vendor($crm_vendor_id): Contact
+    {
+        return $this->importFromCRM($crm_vendor_id, 'vendor');
+    }
+
+
+    /**
+     * @param $crm_id
+     * @param $urlPath
+     * @return Contact
+     * @throws ErrorResponseException
+     */
+    public function importFromCRM ($crm_id, $urlPath): Contact
+    {
+        $data = $this->client->post(
+            'crm/' . $urlPath . '/'. $crm_id . '/import', ['blank'=>'']
+        );
+
+        if (isset($data['code']) && 0 == $data['code']) {
+            return $data['data'] ?? [];
+        }
+
+        if (isset( $data['data']['contact_id'])) {
+            return (new Contacts($this->getClient()))->get($data['data']['contact_id']);
+        }
+
+        throw new ErrorResponseException('Response from Zoho is not success. Message: ' . $data['message'], $data['code'] ?? 500);
+    }
+
+
+}
