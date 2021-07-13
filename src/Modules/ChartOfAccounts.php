fe899a9bf98d8366e957b88d4dd7d7367014adae
Mark Ogilvie <mogilvie@users.noreply.github.com>
2021-07-13 10:33:16 +0100

    Remove copied content from Contact, add ApiKeyName (#82)

--- src/Modules/ChartOfAccounts.php
+++ src/Modules/ChartOfAccounts.php
@@ -2,7 +2,6 @@
 
 namespace Webleit\ZohoBooksApi\Modules;
 use Webleit\ZohoBooksApi\Client;
-use Webleit\ZohoBooksApi\Modules\Contacts\ContactPersons;
 
 /**
  * Class ChartOfAccounts
@@ -10,28 +9,15 @@ use Webleit\ZohoBooksApi\Modules\Contacts\ContactPersons;
  */
 class ChartOfAccounts extends Module
 {
-    /**
-     * @var ContactPersons
-     */
-    public $contactpersons;
-
-    public function __construct(Client $client)
-    {
-        parent::__construct($client);
-
-        $this->contactpersons = new ContactPersons($client);
-    }
 
     /**
-     * @param $id
-     * @return \Illuminate\Support\Collection
+     * Return Zoho API Key Name for Chart of Accounts
      */
-    public function getContactPersons($id)
+    public function getApiKeyName()
     {
-        $className = $this->getModelClassName() . '\\Person';
-        return $this->getPropertyList('contactpersons', $id, $className, 'contact_persons', $this->contactpersons);
+        return 'account_id';
     }
-
+    
     /**
      * @param $id
      * @return bool
