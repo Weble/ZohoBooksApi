e56d213e757e7f595a5d12455af9fa6c16473741
Nahuel Mora <n4huel@users.noreply.github.com>
2022-11-04 06:20:15 -0300

    Fix `ContactPersons::markAsPrimary()` adding random data to avoid a Zoho API REST error when "JSONString" is empty (#97)

--- src/Modules/Contacts/ContactPersons.php
+++ src/Modules/Contacts/ContactPersons.php
@@ -32,14 +32,19 @@ class ContactPersons extends Module
     }
 
     /**
+     * Note that as of 2019-11-27, Zoho Books API errors if you don't
+     * provide a JSONString param. It can't be empty.
+     *
+     * See https://github.com/Weble/ZohoBooksApi/issues/33
+     *
      * @param $id string
      * @return boolean
      */
     public function markAsPrimary($id)
     {
-        $this->client->post($this->getUrl() . '/' . $id . '/primary');
+        $this->client->post($this->getUrl() . '/' . $id . '/primary', ["random" => "data"]);
 
-        // If we arrive here without exceptions, everything went well
+        // If we arrive here without exceptions, everything went well.
         return true;
     }
 }
